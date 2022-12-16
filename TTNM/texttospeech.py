e36cd8bc590a51e8d8f4b950cb97bcfea270a9e9
viethc02 <80147835+viethc02@users.noreply.github.com>
2022-12-16 12:50:17 +0700

    Update texttospeech.py

--- TTNM/texttospeech.py
+++ TTNM/texttospeech.py
@@ -11,11 +11,13 @@ def texttospeech(text, lang1):
         output = gTTS(text, lang=lang1, slow=False)
         output.save("output.mp3")
         playsound.playsound('output.mp3', True)
+        os.remove("output.mp3")
 
     else:
         text = "Please choose correct languages you want"
         output = gTTS(text, lang="en", slow=False)
         output.save("output1.mp3")
         playsound.playsound('output1.mp3', True)
+        os.remove("output1.mp3")
 
 sys.modules[__name__] = texttospeech
